c99870938ac1353f103948e25ef0821676dcdb2e
FLusspferd <julius.gruber@chello.at>
2018-12-02 15:42:03 +0100

    Text component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule

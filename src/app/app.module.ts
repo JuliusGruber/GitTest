6d5bfeb20901e3692f87dbb8da5233e0160de485
FLusspferd <julius.gruber@chello.at>
2018-12-02 16:41:35 +0100

    merge conflicts resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-<<<<<<< HEAD
+
 import { TextComponent } from './text/text.component';
 import { FooterComponent } from './footer/footer.component';
-=======
+
 import { HotfixComponent } from './hotfix/hotfix.component';
->>>>>>> master
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
+
     TextComponent,
-    FooterComponent
-=======
+    FooterComponent,
     HotfixComponent
->>>>>>> master
+
   ],
   imports: [
     BrowserModule

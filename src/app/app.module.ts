93c0143c2fdd2d407304029cb0cf9475b8138734
FLusspferd <julius.gruber@chello.at>
2018-12-02 16:08:20 +0100

    Hotfix component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HotfixComponent } from './hotfix/hotfix.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HotfixComponent
   ],
   imports: [
     BrowserModule

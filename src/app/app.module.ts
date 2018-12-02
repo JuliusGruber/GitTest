ad9098629b908d9b3f89185bf2d1d8e2a439114b
FLusspferd <julius.gruber@chello.at>
2018-12-02 16:15:16 +0100

    Footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TextComponent } from './text/text.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TextComponent
+    TextComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule

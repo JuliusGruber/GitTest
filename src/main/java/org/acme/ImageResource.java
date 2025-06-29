e3eb686535d1e5119ba2e6634c99295aa629aaf9
Flusspferd <julius.gruber@chello.at>
2025-06-29 21:41:44 +0200

    fix import path for MultipartForm

--- src/main/java/org/acme/ImageResource.java
+++ src/main/java/org/acme/ImageResource.java
@@ -11,7 +11,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.jboss.resteasy.reactive.multipart.MultipartForm;
+import org.jboss.resteasy.reactive.server.multipart.MultipartForm;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.metadata.Metadata;
